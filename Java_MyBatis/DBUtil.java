e97d6898d5d27735e43da08539f6967c88adbcec
kidchang93 <lck930721@gmail.com>
2023-10-24 18:02:20 +0900

    MyBatis 로 변환

---
+++ Java_MyBatis/DBUtil.java
@@ -0,0 +1,62 @@
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class DBUtil {
+
+	String url = "jdbc:mariadb://localhost:3306/addr_prj";
+	String user = "root";
+	String pass = "12345";
+	SqlSessionFactory sqlSessionFactory;
+	
+	public void init() {
+		try {
+			String resource = "mybatis-config.xml";
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+		} catch (Exception e) {
+			System.out.println("MyBatis 설정 파일 가져오는 중 문제 발생!!");
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Addr> getAddresses() {
+		SqlSession session = sqlSessionFactory.openSession();
+		AddrMapper mapper = session.getMapper(AddrMapper.class);
+		ArrayList<Addr> addrList = mapper.getAddresses();
+		
+		return addrList;
+	}
+	
+	public void insertAddress(String name, String address, String phone) {
+		SqlSession session = sqlSessionFactory.openSession();
+		AddrMapper mapper = session.getMapper(AddrMapper.class);
+		Addr addr = new Addr(name, address, phone);
+		mapper.insertAddress(addr);
+		
+		session.commit(); // update, delete, insert
+	}
+	
+	public void updateAddress(int id, String name, String address, String phone) {
+		SqlSession session = sqlSessionFactory.openSession();
+		AddrMapper mapper = session.getMapper(AddrMapper.class);
+		Addr addr = new Addr(id, name, address, phone);
+		mapper.updateAddress(addr);
+		
+		session.commit(); // update, delete, insert
+	}
+	
+	public void deleteAddress(int id) {
+		SqlSession session = sqlSessionFactory.openSession();
+		AddrMapper mapper = session.getMapper(AddrMapper.class);
+		mapper.deleteAddress(id);
+		
+		session.commit(); // update, delete, insert
+	}
+	
+}
