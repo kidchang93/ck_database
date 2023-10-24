4e0ca16e4f0717ee73a601f26d28afc52efc95b8
kidchang93 <lck930721@gmail.com>
2023-10-24 18:03:40 +0900

    DB_Test

---
+++ mybatis_db/AddrMapper.java
@@ -0,0 +1,10 @@
+package mybatis_db;
+import java.util.ArrayList;
+
+public interface AddrMapper {
+	
+	public ArrayList<Addr> getAddresses();
+	public void insertAddress(Addr addr);
+	public void updateAddress(Addr addr);
+	public void deleteAddress(int id);
+}
